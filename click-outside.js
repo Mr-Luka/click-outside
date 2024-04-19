cd8d51a55d7ab6e171ffee859fdf7e303612539e
Luka <luka.luka.luka93@gmail.com>
2024-04-19 12:45:58 -0700

    selecting the modals and cards

---
+++ click-outside.js
@@ -0,0 +1,3 @@
+const cardButtons = document.querySelectorAll(".card button");
+const modalOuter = document.querySelector(".modal-outer");
+const modalInner = document.querySelector(".modal-inner");
